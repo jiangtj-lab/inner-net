26c3810b09cedacf621ab3f52003ce2697938902
Mr.J <116749895@qq.com>
2021-08-12 17:25:01 +0800

    init

---
+++ src/main/java/com/jiangtj/example/innernet/InnerNetApplication.java
@@ -0,0 +1,13 @@
+package com.jiangtj.example.innernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiangtj/example/innernet/InnerNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangtj.example.innernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
